4cb4e9864264a3ee4144efdde8d024bf0bce7116
Greg Wilson <gvwilson@third-bit.com>
2026-01-08 19:54:34 -0500

    feat: add Gate class
    closes #5

--- src/asimpy/__init__.py
+++ src/asimpy/__init__.py
@@ -1,5 +1,6 @@
 """Discrete event simulation using async/await."""
 
 from .environment import Environment as Environment
+from .gate import Gate as Gate
 from .process import Process as Process
 from .resource import Resource as Resource
---
+++ src/asimpy/gate.py
@@ -0,0 +1,42 @@
+"""Gate that holds multiple processes until flagged."""
+
+from .actions import BaseAction
+
+
+class Gate:
+    def __init__(self, env):
+        self.env = env
+        self.pending = []
+
+    async def wait(self):
+        await _Wait(self)
+
+    async def release(self):
+        await _Release(self)
+
+
+# ----------------------------------------------------------------------
+
+
+class _Wait(BaseAction):
+    """Wait at the gate."""
+
+    def __init__(self, gate):
+        super().__init__(gate.env)
+        self.gate = gate
+
+    def act(self, coro):
+        self.gate.pending.append(coro)
+
+
+class _Release(BaseAction):
+    """Release processes waiting at gate."""
+
+    def __init__(self, gate):
+        super().__init__(gate.env)
+        self.gate = gate
+
+    def act(self, coro):
+        while self.gate.pending:
+            self.env.schedule(self.env.now, self.gate.pending.pop())
+        self.env.schedule(self.env.now, coro)
